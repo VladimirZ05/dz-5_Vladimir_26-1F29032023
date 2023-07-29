3a3377e1a21228644c0a0095340592d6b26566be
VladimirZ05 <132680480+VladimirZ05@users.noreply.github.com>
2023-07-29 14:11:31 +0600

    Add files via upload

--- App.js
+++ App.js
@@ -1,45 +1,9 @@
-import React, {useState} from "react"
-
-const Counter = () => {
-    const [count, setCount] = useState(0);
-
-    const increament = () => {
-        setCount(count + 1);
-    };
-    const decrement = () => {
-        setCount(count - 1);
-    };
-    return (
+import {Counter, InputForm} from "./mini-project";
+export default function App(){
+    return(
         <div>
-            <h2>Counter</h2>
-            <p>Count: {count}</p>
-            <button onClick={increament}>+1</button>
-            <button onClick={decrement}>-1</button>
+            <Counter />
+            <InputForm />
         </div>
     );
-};
-export default Counter;
-
-
-// const InputForm = () => {
-//     const [inputValue, setInputValue] = useState('');
-//
-//     const handleChange = (event) => {
-//         setInputValue(event.target.value);
-//     };
-//     const handleSubmit = (event) => {
-//         event.preventDefault();
-//         alert(`Submitted value ${inputValue}`);
-//         setInputValue('')
-//     };
-//     return (
-//         <div>
-//             <h2>Input Form</h2>
-//             <form onSubmit={handleSubmit}>
-//                 <input type="text" value={inputValue} onChange={handleChange}/>
-//                 <button type={"submit"}>Submit</button>
-//             </form>
-//         </div>
-//     );
-// };
-// export default InputForm
+}
